d66b06fd8f19133b16d13f6c7ad41d2a5b36ade7
Steve W Home <smweis@gmail.com>
2019-08-06 16:45:15 -0400

    removed sensitive files

--- scripts/RM_Anova.R
+++ scripts/RM_Anova.R
@@ -1,4 +1,6 @@
-myData <- data_for_R
+dataForRT_for_R <- read.csv("C:/Users/smwei/Dropbox/Penn Post Doc/Frisbee_Ref_Frames/analysis/Ultimate/scripts/dataForRT_for_R.csv")
+
+myData <- dataForRT_for_R
 
 myData.mean <- aggregate(myData$Trials_responsert,
                          by = list(myData$participant, myData$stimLoc,
@@ -18,18 +20,28 @@ rt.aov <- with(myData.mean,
 
 
 library(ez)
+library(lme4)
 
-ezANOVA(data = data_for_R, dv=Trials_responsert, wid = participant, within=c(promptType,stimLoc,condition))
-friz <- data_for_R[data_for_R$condition == "friz",]
-nav <- data_for_R[data_for_R$condition == "nav",]
+ezANOVA(data = dataForRT_for_R, dv=Trials_responsert, wid = participant, within=c(promptType,stimLoc,condition))
+friz <- dataForRT_for_R[dataForRT_for_R$condition == "Frisbee",]
+nav <- dataForRT_for_R[dataForRT_for_R$condition == "Navigation",]
 ezANOVA(data = friz, dv=Trials_responsert, wid = participant, within=c(promptType,stimLoc))
 ezANOVA(data = nav, dv=Trials_responsert, wid = participant, within=c(promptType,stimLoc))
 
 
 
         
-unrestricted_fit <- lmer(Trials_responsert ~ stimLoc * condition * promptType + (1 | participant), data=data_for_R, REML=F)
+unrestricted_fit <- lmer(Trials_responsert ~ stimLoc * condition * promptType + (1 | participant), data=dataForRT_for_R, REML=F)
 #next, compute a model where the effect of status is not estimated
-restricted_fit <- lmer(Trials_responsert ~ condition + (1 | participant), data=data_for_R, REML=F)
+restricted_fit <- lmer(Trials_responsert ~ condition + (1 | participant), data=dataForRT_for_R, REML=F)
 #compute the AIC-corrected log-base-2 likelihood ratio (a.k.a. "bits" of evidence)
 (AIC(restricted_fit)-AIC(unrestricted_fit))*log2(exp(1))
+
+
+
+
+
+
+ezANOVA(data = dataForRT_for_R, dv=Trials_responsert, wid = participant, within=c(promptType,stimLoc,condition),between = Sex)
+
+
